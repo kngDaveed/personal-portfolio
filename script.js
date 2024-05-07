dc9ea917a244816d2791563f390431b523d27bba
kngDaveed <davidgodfrey365@gmail.com>
2024-05-07 03:13:11 +0100

    all

--- script.js
+++ script.js
@@ -8,33 +8,35 @@ menuIcon.onclick = () => {
 };
 
 /*=======for active links web layout==========*/ 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
+//         if(top >= offset && top < offset + height) {
+//             navLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
 
     /*=======sticky navbar==========*/ 
-    let header = document.querySelector('header');
+    // let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+    // header.classList.toggle('sticky', window.scrollY > 100);
 
     /*=======to rempve the open burger-menu when a link is clicked or we scroll==========*/ 
-    menuIcon.classList.remove('bx-message-square-x');
-    navbar.classList.remove('active');
-};
+//     menuIcon.classList.remove('bx-message-square-x');
+//     navbar.classList.remove('active');
+// };
+
+
 
    /*=======scroll reveal==========*/ 
    ScrollReveal({
@@ -48,6 +50,8 @@ window.onscroll = () => {
 ScrollReveal().reveal('.home-img, .about-img, .skills-container, .portfolio-box, .central', { origin:'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-content h2, .about-content h3', { origin:'left' });
 ScrollReveal().reveal('.about-content h2, .about-content h3', { origin:'right' });
+
+
 
 /*=======typed.js text==========*/ 
 const typed = new Typed('.multiple-text', {
